f1d541cee3acaff2f0916e3b920d79b4be3fb017
Tazohcliff <tazohcliff@gmail.com>
2025-04-24 17:26:56 +0100

    Database set up

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-import createError from 'http-errors';
 import express from 'express';
-import path from 'path';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import morgan from 'morgan';
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+import winstonLogger from "./utils/logger.js"
 
-var app = express();
+
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
+
+const app = express();
+
+const __filname = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filname)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+
+const morganFormat = process.env.NODE_ENV === "production" ? "dev" : 'combined'
+app.use(morgan(morganFormat, { stream: winstonLogger.stream }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -22,20 +31,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+export default app
---
+++ config/db.js
@@ -0,0 +1,127 @@
+import pg from "pg"
+import logger from "../utils/logger.js"
+
+
+const { Pool } = pg
+
+
+const { PGUSER, PGPASSWORD, PGHOST, PGNAME, PGPORT, NODE_ENV } = process.env
+
+
+
+if (!PGHOST || !PGPASSWORD || !PGNAME || !PGUSER || !PGPORT) {
+  logger.error("Database environment variables are missing! Check your .env file.")
+  process.exit(1)
+}
+
+const pool = new Pool({
+  user: PGUSER,
+  host: PGHOST,
+  database: PGNAME,
+  password: PGPASSWORD,
+  port: parseInt(PGPORT, 10),
+  connectionTimeoutMillis: 2000
+})
+
+logger.info(`Database is configured for: ${PGNAME}`)
+
+pool.on("connect", (client) => {
+  logger.info(`Client connected from Pool (Total count: ${pool.totalCount}`)
+})
+
+pool.on("error", (err, client) => {
+  logger.error('Unexpected error on idle client in pool', err)
+  process.exit(-1)
+})
+
+const initialzeDbSchema = async () => {
+  const client = await pool.connect();
+  try {
+    logger.info("Initializing database schema...");
+    await client.query("CREATE EXTENSION IF NOT EXISTS pgcrypto");
+
+    // 1) Client table
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS client (
+        clientId         UUID    PRIMARY KEY DEFAULT gen_random_uuid(),
+        first_name       VARCHAR(50)  NOT NULL,
+        last_name        VARCHAR(50)  NOT NULL,
+        email            VARCHAR(255) UNIQUE NOT NULL,
+        password         VARCHAR(255) NOT NULL,
+        profile_image_url VARCHAR(255)
+      );
+    `);
+    logger.info("client table has been created");
+
+    // 2) Service provider table
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS service_provider (
+        spId              UUID    PRIMARY KEY DEFAULT gen_random_uuid(),
+        email             VARCHAR(255) UNIQUE NOT NULL,
+        password          VARCHAR(255) NOT NULL,
+        profile_image_url VARCHAR(255),
+        profession        VARCHAR(50)  NOT NULL,
+        description       VARCHAR(255) NOT NULL,
+        booked            BOOLEAN
+      );
+    `);
+    logger.info("service_provider table has been created");
+
+    // 3) Timeslot table
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS timeslot (
+        id          UUID    PRIMARY KEY DEFAULT gen_random_uuid(),
+        workingDays VARCHAR(50) NOT NULL,
+        workingTime VARCHAR(50) NOT NULL,
+        spId        UUID    NOT NULL DEFAULT gen_random_uuid(),
+        FOREIGN KEY (spId) REFERENCES service_provider(spId)
+      );
+    `);
+    logger.info("timeslot table has been created");
+
+    // 4) Appointment table
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS appointment (
+        id        UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        spId      UUID NOT NULL DEFAULT gen_random_uuid(),
+        clientId  UUID NOT NULL DEFAULT gen_random_uuid(),
+        FOREIGN KEY (clientId) REFERENCES client(clientId),
+        FOREIGN KEY (spId)     REFERENCES service_provider(spId)
+      );
+    `);
+    logger.info("appointment table has been created");
+
+  } catch (error) {
+    logger.error(`Error while initializing the schema`, error);
+    process.exit(1);
+  } finally {
+    client.release();
+  }
+};
+
+
+
+const connectToDb = async () => {
+  try {
+    const client = await pool.connect()
+    logger.info(`Database connection pool established successfully`)
+    client.release()
+  } catch (error) {
+    logger.error('Unable to establish database connection pool', error)
+    process.exit(1)
+  }
+}
+const query = async (text, params) => {
+  const start = Date.now()
+  try {
+    const response = await pool.query(text, params)
+    const duration = Date.now() - start;
+    logger.info(`Executed query: { text: ${text.substring(0, 100)}..., params: ${JSON.stringify(params)}, duration: ${duration}ms, rows: ${response.rowCount}}`);
+    return response
+  } catch (error) {
+    logger.error(`Error executing query: { text: ${text.substring(0, 100)}..., params: ${JSON.stringify(params)}, error: ${error.message}}`);
+    throw error
+  }
+}
+
+export { pool, connectToDb, query, initialzeDbSchema }
